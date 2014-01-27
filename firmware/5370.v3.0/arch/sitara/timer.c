df0e0e91bf8c1e69317765541d19d39e4bb257ce
jks-prv <jks@jks.com>
2014-01-28 11:22:56 +1300

    add microsecond timer routine

--- firmware/5370.v3.0/arch/sitara/timer.c
+++ firmware/5370.v3.0/arch/sitara/timer.c
@@ -5,9 +5,18 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-u4_t sys_now(void) {
+u4_t sys_now(void)
+{
 	struct timeval tv;
 
 	gettimeofday(&tv, 0);
     return tv.tv_sec*1000 + tv.tv_usec/1000;
+}
+
+u4_t sys_now_us(void)
+{
+	struct timeval tv;
+
+	gettimeofday(&tv, 0);
+    return tv.tv_sec*1000000 + tv.tv_usec;	// ignore overflow
 }
